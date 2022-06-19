16bcef50b8ee71746f94327ef72fcbdbf4b970c5
desth-x <david.estrada@null.net>
2022-06-19 11:20:28 -0500

    Multiple Files Feature

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-import colorsys
+from time import sleep
 from PIL import Image #pip install pillow
+from PIL import ImageFont, ImageDraw 
+import os
 
-def convertImageToCharacters(url):
-    image = Image.open(url)
+def convertImageToCharacters(urlSrc, urlTrj, booleanSaveAsImage):
+    image = Image.open(urlSrc)
+    image.filename
     pixels = image.load()
     chars = ['@', '%', '&', '#', '0', 'C', '?', '!', ':', '.', ' '] #Ordered list, the first char is the one with the most of fill
     deepness = len(chars)-1 
     strResult =""
+    ##The following validation is in order to create an image as result instead of a txt
+    if(booleanSaveAsImage):                                                           ##
+        text_font = ImageFont.truetype("C:\Windows\Fonts\lucon.ttf", 12)              ##
+        text_width = text_font.getmask(("t "*image.size[0])).getbbox()[2]             ##
+        canvas = Image.new('RGB', (text_width + 30, 14 * image.size[1]), 'white')     ##
+        image_editable = ImageDraw.Draw(canvas)                                       ##
+    ####################################################################################
     for y in range(image.size[1]):
         strRow = ""
         for x in range(image.size[0]):
@@ -18,19 +28,47 @@ def convertImageToCharacters(url):
             convertedValue = int(round(k * factor, 0)) #This value is in the range of the amount of characters in the list.
             strRow += chars[convertedValue] + " "
         strResult += strRow + "\n"
-    return strResult
+        ##Paints the text in the canvas, in case that the result is an image ###########
+        if(booleanSaveAsImage):                                                       ##
+            image_editable.text( (15, y*14), strRow, (0, 0, 0), font=text_font)       ##
+        ################################################################################
+    imageName = image.filename.split("\\")[1].split(".")[0]
+    if booleanSaveAsImage:
+        canvas.save(urlTrj+"\\"+imageName+".png")
+    else:
+        open(urlTrj+"\\"+imageName+".txt", "w").write(strResult)
+
+def readFiles(booleanSaveAsImage):
+    directory = os.listdir("input")
+    if len(directory) == 0:
+        print("No files were found at the input folder")
+        return
+    for element in directory:
+        strUrlScr = "input\\" + element
+        try:
+            print("Converting "+element)
+            convertImageToCharacters(strUrlScr, "output", booleanSaveAsImage)            
+        except:
+            print("Skipping " + strUrlScr)                
+    print("\033[92m---> Files created!\033[0m")
 
 def readUserInput():
     try:
-        print("\033[96m---------------------------------------")        
-        print("Info: type 'exit' to stop this proccess.")        
-        userInput = input("\033[96mImage Location (Path):\033[0m")
-        if userInput != "exit":
-            f = open("output.txt", "w") #The file to save the result
-            f.write(convertImageToCharacters(userInput)) #I highly recommed to use small images since there is no scaling, (300 x 300 around).
-            print("\033[92m---> File created!\033[0m")
+        print("\033[96m--------------------------------")  
+        print("Type '1' for TXT or '2' for PNG")
+        print("--------------------------------")  
+        userInput = input("\033[96mInput: \033[0m")
+        if userInput == "1":            
+            readFiles(False)
+            input("\033[96mPress Enter to finish\033[0m")
+        elif userInput == "2":
+            readFiles(True)
+            input("\033[96mPress Enter to finish\033[0m")
+        else:
+            print("\033[91mUnknown Option, try again.\033[0m")
+            readUserInput()
+        
     except:
-        print("\033[91mThere was an error, try again.\033[0m")
-        readUserInput()
+        print("\033[91mThere was an error.\033[0m")        
 
 readUserInput()
